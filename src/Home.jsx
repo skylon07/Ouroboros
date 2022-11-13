ca91498d2ceec7cdbe2a2594619a342facbab296
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-11-12 22:12:11 -0700

    Add basic React Router setup

---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+    return <>
+        <h1>Welcome Home!</h1>
+        <Link to="/about">About</Link>
+    </>
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+
+import Home from './Home'
+import About from './About'
+
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/about"
+                    element={<About />}
+                />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 )
 
