e46f0c312c0238d71fbf7e29dd07aed03b60c552
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-03 02:29:03 -0700

    Improve "no file selected" error message

--- src/shared/PageApi.js
+++ src/shared/PageApi.js
@@ -13,8 +13,16 @@ export default class PageApi {
     }
 
     async updatePyFile(pyFile) {
-        const fileDataAsStr = await readFile(pyFile)
-        return await axios.post(this._apiBase, {pyfile: fileDataAsStr})
+        try {
+            const fileDataAsStr = await readFile(pyFile)
+            return await axios.post(this._apiBase, {pyfile: fileDataAsStr})
+        } catch (error) {
+            if (error.message.includes("Failed to execute 'readAsText' on 'FileReader': parameter 1 is not of type 'Blob'")) {
+                throw new Error("No file selected")
+            } else {
+                throw error
+            }
+        }
     }
 
     listenForConsole() {
