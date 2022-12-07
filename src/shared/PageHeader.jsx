40c1651c9761b124d0f1323f47a08fc78fefa6af
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-07 01:24:12 -0700

    Add driver documentation (button and text)

--- src/shared/PageHeader.jsx
+++ src/shared/PageHeader.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
 import { Link } from "react-router-dom"
 
-export default function PageHeader({title}) {
+import { readFile } from "shared/files"
+
+import './PageHeader.css'
+
+export default function PageHeader({title, docRef}) {
+    const [docShown, setDocShown] = useState(false)
+    const showDoc = () => setDocShown(true)
+    const hideDoc = () => setDocShown(false)
+
+    const [docText, setDocText] = useState(null)
+    const docTextReadRef = useRef(false)
+    useEffect(() => {
+        if (!docTextReadRef.current) {
+            const asyncFn = async () => {
+                const docFileResponse = await fetch(docRef)
+                const docText = await docFileResponse.text()
+                setDocText(docText)
+            }
+            asyncFn()
+            docTextReadRef.current = true
+        }
+    })
+
+    const docVisibleClass = docShown ? "" : "hidden"
+
     return <div className="PageHeader">
-        <Link to="/">Back to Home</Link>
+        <div className="PageHeader-TopBar">
+            <Link to="/">Back to Home</Link>
+            <spacer />
+            <button onClick={showDoc}>Show Driver Docs</button>
+        </div>
         <h1>{title}</h1>
+        <div className={`PageHeader-DocViewer ${docVisibleClass}`}>
+            <button onClick={hideDoc}>✕</button>
+            <pre>{docText}</pre>
+        </div>
     </div>
 }
