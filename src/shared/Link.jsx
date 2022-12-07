c178d6672b576801f13c4ec14d3f08c29e626157
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-07 11:09:22 -0700

    Replace react router <Link />s with custom ones

---
+++ src/shared/Link.jsx
@@ -0,0 +1,11 @@
+import { Link as RLink } from 'react-router-dom'
+
+import './Link.css'
+
+export default function Link({to, children}) {
+    return <div className="Link">
+        <RLink to={to}>{children}</RLink>
+        <div className="Link-Nonhover">{children}</div>
+    </div>
+
+}
