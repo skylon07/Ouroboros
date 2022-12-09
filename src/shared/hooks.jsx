b41809331e3f006ed104bf7c0bac4877211424e5
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-09 13:20:07 -0700

    Add hook to help with PageApi calls

--- src/shared/hooks.jsx
+++ src/shared/hooks.jsx
@@ -1,4 +1,21 @@
-const { useRef, useEffect } = require("react")
+import { useRef, useEffect, useState } from 'react'
+
+export function usePlainPageApiCallback(pageApiFn) {
+    const pageApiFnRef = useRef(pageApiFn)
+    pageApiFnRef.current = pageApiFn
+    const [value, setValue] = useState(null)
+    useEffect(() => {
+        if (value === null) {
+            const asyncFn = async () => {
+                const newValue = await pageApiFnRef.current()
+                setValue(newValue)
+            }
+            asyncFn()
+        }
+    }, [value])
+    const invalidate = () => setValue(null)
+    return [value, invalidate]
+}
 
 export function usePageApi(PageApiClass) {
     const apiRef = useRef(null)
