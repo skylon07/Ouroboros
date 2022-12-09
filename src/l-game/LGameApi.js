b81022d2af4acfa7239e16aba0186377a311a37e
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-09 15:08:08 -0700

    Finish client-side API stubs

--- src/l-game/LGameApi.js
+++ src/l-game/LGameApi.js
@@ -1,51 +1,82 @@
 import PageApi from 'shared/PageApi'
 
+import { PlayerPosition, Position } from './gamestate'
+
 export default class LGameApi extends PageApi {
     constructor() {
         super('/l-game')
     }
 
     async fetchPlayerTurn() {
-        // TODO
+        const response = await this._callGetAction('/turn/player')
+        return response.player
     }
 
     async fetchMoveMode() {
-        // TODO
+        const response = await this._callGetAction('/turn/mode')
+        return response.mode
     }
 
     async advanceTurnCycle() {
-        // TODO
+        await this._callPostAction('/turn', {})
     }
 
     async fetchPlayerPieces() {
-        // TODO
+        const {bluePositionPath, redPositionPath} = await this._callGetAction('/pieces/players')
+        return {
+            bluePlayerPiecePosition: PlayerPosition.fromPositionPath(
+                bluePositionPath.map((positionRep) => 
+                    new Position(positionRep.row, positionRep.col)
+                )
+            ),
+            redPlayerPiecePosition: PlayerPosition.fromPositionPath(
+                redPositionPath.map((positionRep) => 
+                    new Position(positionRep.row, positionRep.col)
+                )
+            ),
+        }
     }
 
     async setActivePlayerPosition(newPosition) {
-        // TODO
+        await this._callPostAction('/pieces/activeplayer', {
+            positionPath: newPosition.toPositionPath().map((position) => {
+                return {
+                    row: position.rowIdx,
+                    col: position.colIdx,
+                }
+            })
+        })
     }
 
     async fetchTokenPieces() {
-        // TODO
+        const {token1Position, token2Position} = await this._callGetAction('/pieces/tokens')
+        return {
+            tokenPiece1Position: new Position(token1Position.row, token1Position.col),
+            tokenPiece2Position: new Position(token2Position.row, token2Position.col),
+        }
     }
 
     async setTokenPiecePosition(tokenNum, newPosition) {
-        // TODO
+        await this._callPostAction('/pieces/token', {
+            id: tokenNum,
+            position: {
+                row: newPosition.rowIdx,
+                col: newPosition.colIdx,
+            },
+        })
     }
 
     async fetchGameOver() {
-        // TODO
-    }
-
-    async recalculateGameOver() {
-        // TODO
+        const response = await this._callGetAction('/game-over')
+        return response.isGameOver
     }
 
     async notifyOutOfTime() {
-        // TODO
+        await this._callPostAction('/turn/out-of-time', {})
     }
 
     async fetchWinningPlayer() {
-        // TODO
+        const response = await this._callGetAction('/game-over/winner')
+        return response.player
     }
 }
