d6b1edbd768df282537a0e6f8287f59d75e0b536
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-23 11:24:49 -0700

    Add server-side dots and boxes API

--- server/app_apis/__init__.py
+++ server/app_apis/__init__.py
@@ -1,7 +1,9 @@
 from .console.ConsoleApi import ConsoleApi
 from .l_game.LGameApi import LGameApi
+from .dots_and_boxes.DotsAndBoxesApi import DotsAndBoxesApi
 
 apis_by_path = {
     '/console': ConsoleApi(),
     '/l-game': LGameApi(),
+    '/dots-and-boxes': DotsAndBoxesApi(),
 }
---
+++ server/app_apis/dots_and_boxes/DotsAndBoxesApi.py
@@ -0,0 +1,125 @@
+from ..ServerApi import ServerApi
+from ..dots_and_boxes import driver
+
+
+class DotsAndBoxesApi(ServerApi):
+    def __init__(self):
+        super().__init__("/dots-and-boxes", driver)
+        self._useDefaultImplementation = True
+        self._gameState = None
+        self._initGameState()
+        driver._syncApi(self)
+        self._defaultResetGame()
+
+    def _initGameState(self):
+        self._gameState = {
+            'playerTurn': None,
+            'boardState': None,
+            'gameOver': None,
+            'winningPlayer': None,
+            'menuState': None,
+            'menuData': None,
+        }
+
+    def onPostFile(self, queryParams, requestDict):
+        self._useDefaultImplementation = False
+        self._initGameState()
+
+    def getAction(self, action, queryParams):
+        if action == '/game-state':
+            return self._gameState
+        else:
+            return ValueError("Invalid action path")
+
+    def postAction(self, action, queryParams, requestDict):
+        if action == '/board':
+            boardShape = requestDict['boardShape']
+            if self._useDefaultImplementation:
+                self._defaultSelectBoard(boardShape)
+            else:
+                self._runInContext(lambda: self._selectBoard(boardShape))
+        elif action == '/new-board':
+            newBoardShape = requestDict['newBoardShape']
+            if self._useDefaultImplementation:
+                self._defaultRecordNewBoard(newBoardShape)
+            else:
+                self._runInContext(lambda: self._recordNewBoard(newBoardShape))
+        elif action == '/reset':
+            if self._useDefaultImplementation:
+                self._defaultResetGame()
+            else:
+                self._runInContext(lambda: self._resetGame())
+        elif action == '/editor-activate':
+            if self._useDefaultImplementation:
+                self._defaultActivateEditor()
+            else:
+                self._runInContext(lambda: self._activateEditor())
+        elif action == '/selector-activate':
+            if self._useDefaultImplementation:
+                self._defaultActivateBoardSelector()
+            else:
+                self._runInContext(lambda: self._activateBoardSelector())
+        elif action == '/menu-cancel':
+            if self._useDefaultImplementation:
+                self._defaultCancelMenu()
+            else:
+                self._runInContext(lambda: self._cancelMenu())
+        elif action == '/menu-submit':
+            if self._useDefaultImplementation:
+                self._defaultSubmitMenu()
+            else:
+                self._runInContext(lambda: self._submitMenu())
+        else:
+            return ValueError("Invalid action path")
+
+    # default implementations
+    def _defaultSelectBoard(self, boardShape):
+        pass # TODO
+
+    def _defaultRecordNewBoard(self, newBoardShape):
+        pass # TODO
+
+    def _defaultResetGame(self):
+        pass # TODO
+
+    def _defaultActivateEditor(self):
+        pass # TODO
+
+    def _defaultActivateBoardSelector(self):
+        pass # TODO
+
+    def _defaultCancelMenu(self):
+        pass # TODO
+
+    def _defaultSubmitMenu(self, menuData):
+        pass # TODO
+
+    # overridden implementations
+    def _selectBoard(self, boardShape):
+        return NotImplemented
+
+    def _recordNewBoard(self, newBoardShape):
+        return NotImplemented
+
+    def _resetGame(self):
+        return NotImplemented
+
+    def _activateEditor(self):
+        return NotImplemented
+
+    def _activateBoardSelector(self):
+        return NotImplemented
+
+    def _cancelMenu(self):
+        return NotImplemented
+
+    def _submitMenu(self, menuData):
+        return NotImplemented
+
+
+    # required subclass method overrides
+    def _getGlobals(self):
+        return globals()
+
+    def _exec_noVarsInContext(self, fileData, globalVars, localVars):
+        exec(fileData, globalVars, localVars)
