e7d47ca4025c6c9f38feec1d58301e06dc6fb132
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-24 12:57:27 -0700

    Register TicTacToe api with url

--- server/app_apis/__init__.py
+++ server/app_apis/__init__.py
@@ -1,9 +1,11 @@
 from .console.ConsoleApi import ConsoleApi
+from .tic_tac_toe.TicTacToeApi import TicTacToeApi
 from .l_game.LGameApi import LGameApi
 from .dots_and_boxes.DotsAndBoxesApi import DotsAndBoxesApi
 
 apis_by_path = {
     '/console': ConsoleApi(),
+    '/tic-tac-toe': TicTacToeApi(),
     '/l-game': LGameApi(),
     '/dots-and-boxes': DotsAndBoxesApi(),
 }
